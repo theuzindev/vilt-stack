728dc5dad826d81f6e8f5e9bdfd5c1eefea77c92
Matheus Nascimento <matheus.nascimento@devsquad.com>
2023-05-11 18:00:04 -0300

    apply formatter

--- resources/src/app.ts
+++ resources/src/app.ts
@@ -1,23 +1,27 @@
-import './bootstrap';
-import '../css/app.css';
+import '../css/app.css'
+import './bootstrap'
 
-import { createApp, h, DefineComponent } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { createInertiaApp } from '@inertiajs/vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { DefineComponent, createApp, h } from 'vue'
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m'
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel'
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue, Ziggy)
-            .mount(el);
-    },
-    progress: {
-        color: '#4B5563',
-    },
-});
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) =>
+    resolvePageComponent(
+      `./Pages/${name}.vue`,
+      import.meta.glob<DefineComponent>('./Pages/**/*.vue')
+    ),
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(ZiggyVue, Ziggy)
+      .mount(el)
+  },
+  progress: {
+    color: '#4B5563'
+  }
+})
--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,27 @@
-import vue from "@vitejs/plugin-vue";
-import laravel from "laravel-vite-plugin";
-import path from "path";
-import { defineConfig } from "vite";
+import vue from '@vitejs/plugin-vue'
+import laravel from 'laravel-vite-plugin'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: "resources/src/app.ts",
-            ssr: "resources/src/ssr.ts",
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "resources/src"),
-        },
-    },
-});
+  plugins: [
+    laravel({
+      input: 'resources/src/app.ts',
+      ssr: 'resources/src/ssr.ts',
+      refresh: true
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false
+        }
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'resources/src')
+    }
+  }
+})
