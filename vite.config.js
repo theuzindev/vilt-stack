70ac4f2ef0b070c9d219c2625336c161414647fb
Matheus Nascimento <matheus.nascimento@devsquad.com>
2023-05-11 23:51:27 -0300

    add vue macros

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,27 @@
-import vue from '@vitejs/plugin-vue'
-import laravel from 'laravel-vite-plugin'
-import path from 'path'
+import Vue from '@vitejs/plugin-vue'
+import Laravel from 'laravel-vite-plugin'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import VueMacros from 'unplugin-vue-macros/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
-    laravel({
+    Laravel({
       input: 'resources/src/app.ts',
       ssr: 'resources/src/ssr.ts',
       refresh: true
     }),
-    vue({
-      template: {
-        transformAssetUrls: {
-          base: null,
-          includeAbsolute: false
-        }
+    VueMacros({
+      plugins: {
+        vue: Vue({
+          template: {
+            transformAssetUrls: {
+              base: null,
+              includeAbsolute: false
+            }
+          }
+        })
       }
     }),
     AutoImport({
@@ -29,10 +33,5 @@ export default defineConfig({
       dirs: ['resources/src/Components', 'resources/src/Layouts'],
       dts: 'resources/src/Types/components.d.ts'
     })
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'resources/src')
-    }
-  }
+  ]
 })
