1d48ad4fe38b93c4b92420a51a5bbe148430f724
Matheus Nascimento <matheus.nascimento@devsquad.com>
2023-05-12 17:24:24 -0300

    refactor camelCase

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import Vue from '@vitejs/plugin-vue'
-import Laravel from 'laravel-vite-plugin'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
+import vue from '@vitejs/plugin-vue'
+import laravel from 'laravel-vite-plugin'
+import autoImport from 'unplugin-auto-import/vite'
+import components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
-    Laravel({
+    laravel({
       input: 'resources/src/app.ts',
       ssr: 'resources/src/ssr.ts',
       refresh: true
     }),
-    Vue({
+    vue({
       template: {
         transformAssetUrls: {
           base: null,
@@ -19,11 +19,11 @@ export default defineConfig({
         }
       }
     }),
-    AutoImport({
+    autoImport({
       imports: ['vue', '@vueuse/core'],
       dts: 'resources/src/Types/auto-imports.d.ts'
     }),
-    Components({
+    components({
       dirs: ['resources/src/Components', 'resources/src/Layouts'],
       dts: 'resources/src/Types/components.d.ts'
     })
