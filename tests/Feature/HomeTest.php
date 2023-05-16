d671ed649978150d5ba1b6e69c96420ba8e4fcae
Matheus Nascimento <matheus.nascimento@devsquad.com>
2023-05-15 21:20:12 -0300

    add test for home

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Inertia\Testing\AssertableInertia as Assert;
+
+use function Pest\Laravel\get;
+
+it('should render home page', function () {
+    $response = get('/');
+    $response->assertOk()
+        ->assertInertia(fn (Assert $page) => $page
+            ->component('Home/Index'));
+});
